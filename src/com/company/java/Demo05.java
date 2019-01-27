c5e7ee69801f8c5b9d899c24b00da7971d161fb6
e <lambert1129@foxmail.com>
2019-01-27 17:34:48 +0800

    接口

--- src/com/company/java/Demo05.java
+++ src/com/company/java/Demo05.java
@@ -1,4 +1,35 @@
 package com.company.java;
 
+import com.company.java.model.Displayable;
+import com.company.java.model.News;
+import com.company.java.model.Video;
+
+/*
+接口
+ */
 public class Demo05 {
+    public static void main(String[] args){
+
+        News news = new News("abc","abc1");
+        Video video = new Video();
+
+        viewVideo(news);
+        viewNews(video);
+
+
+    }
+    private static void viewNews(Displayable item){
+        item.display();
+        System.out.print("播放完毕");
+    }
+
+    private static void viewVideo(News item){
+        item.display();
+        System.out.print("播放完毕");
+    }
+
+    private static void viewLecture(Video item){
+        item.display();
+        System.out.print("播放完毕");
+    }
 }
