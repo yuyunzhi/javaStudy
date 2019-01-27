18c367a5b59e4eb85ba3b5881462df69717919e9
e <lambert1129@foxmail.com>
2019-01-27 12:52:59 +0800

    新增Demo2

---
+++ src/com/company/java/Demo02.java
@@ -0,0 +1,9 @@
+package com.company.java;
+
+import java.io.IOException;
+
+public class Demo02 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("123");
+    }
+}
