ed0fe895d0e8be94c55d5c60e307378213f13fb1
e <lambert1129@foxmail.com>
2019-01-27 17:34:30 +0800

    抽象

--- src/com/company/java/Demo04.java
+++ src/com/company/java/Demo04.java
@@ -2,7 +2,12 @@ package com.company.java;
 
 import com.company.java.model.FileNewsReader;
 
-//抽象类  父类 为 NewsReader  子类为 UrlNewsReader FileNewsReader
+/*
+抽象类
+父类:NewsReader
+子类:UrlNewsReader FileNewsReader
+子类override父类的abstract的函数
+ */
 public class Demo04 {
     public static void main(String[] args){
         FileNewsReader fileNewsReader = new FileNewsReader();
