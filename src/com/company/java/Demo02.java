034fb2f58dead4ac78a03df31fcb1f7277927489
e <lambert1129@foxmail.com>
2019-01-27 17:33:30 +0800

    封装

--- src/com/company/java/Demo02.java
+++ src/com/company/java/Demo02.java
@@ -3,12 +3,12 @@ package com.company.java;
 import com.company.java.model.News;
 
 /*
-封装一个类，用来传入title ,content初始化内容
+封装类，用来传入title ,content初始化内容
  */
 public class Demo02 {
     public static void main(String[] args){
 
         News news = new News("标题啊","内容啊");
-        System.out.println(news.display());
+        news.display();//无返回值哦~
     }
 }
---
+++ src/com/company/java/model/Displayable.java
@@ -0,0 +1,4 @@
+package com.company.java.model;
+
+public interface Displayable {
+}
