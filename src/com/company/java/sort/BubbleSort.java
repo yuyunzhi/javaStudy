356e5eaa72dc568d555db7193be98aeeffe4e6d4
e <lambert1129@foxmail.com>
2019-01-29 22:53:32 +0800

    冒泡排序代码

--- src/com/company/java/sort/BubbleSort.java
+++ src/com/company/java/sort/BubbleSort.java
@@ -1,4 +1,23 @@
 package com.company.java.sort;
 
 public class BubbleSort {
+
+    public int[] bubbleSort(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 1; j < (len - i); j++) {
+
+                if (nums[j - 1] > nums[j]) {
+
+                    int temp = nums[j - 1];
+                    nums[j - 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+
+
+
 }
