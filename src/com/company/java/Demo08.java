2b56e2803d4c415e0b587c83c5f5980e44938b86
e <lambert1129@foxmail.com>
2019-01-29 22:53:03 +0800

    排序

---
+++ src/com/company/java/Demo08.java
@@ -0,0 +1,19 @@
+package com.company.java;
+
+import com.company.java.sort.BubbleSort;
+
+import java.util.Arrays;
+
+/**
+ * 排序
+ */
+public class Demo08 {
+    public static void main(String[] args){
+
+        //冒泡排序
+        int[] nums=new int[]{5,1,7,3,9,2,0,3};
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] newNums =bubbleSort.bubbleSort(nums);
+        System.out.println(Arrays.toString(newNums));
+    }
+}
---
+++ src/com/company/java/sort/BubbleSort.java
@@ -0,0 +1,4 @@
+package com.company.java.sort;
+
+public class BubbleSort {
+}
