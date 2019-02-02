54266c812eae1189cb15405584a87c5c66d215e9
e <lambert1129@foxmail.com>
2019-02-02 17:12:58 +0800

    泛型

--- src/com/company/java/Demo06.java
+++ src/com/company/java/Demo06.java
@@ -5,7 +5,7 @@ import com.company.java.model.News;
 import com.company.java.model.Video;
 
 /**
- * 多态
+ * 多态：父类型或接口中的同一个方法, 在子类型或接口的具体对象里, 有不同实现方法!
  */
 public class Demo06 {
 
---
+++ src/com/company/java/Demo10.java
@@ -0,0 +1,4 @@
+package com.company.java;
+
+public class Demo10 {
+}
